9c8f6dfb3e69dde179eb6359bf874108c58a0b73
t0kw0u <t0kw0u@SZA0030883>
2022-11-30 16:09:50 +0100

    10 és 11 hét

---
+++ T0KW0U_11HET/Controllers/BoatController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using T0KW0U_10HET.Models;
+
+namespace T0KW0U_11HET.Controllers
+{
+    //[Route("api/[controller]")]
+    [ApiController]
+
+    public class BoatController : Controller
+    {
+
+        [HttpGet]
+        [Route("questions/all")]
+        public IActionResult MindegyHogyHivjak()
+        {
+
+            HajosContext context = new HajosContext();
+            var kérdések = from x in context.Questions
+                           select x.Question1;
+
+            return Ok(kérdések);
+        }
+    }
+}
