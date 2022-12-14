bde47a7e0ab2423af2738e71015d487e3cc8b97e
t0kw0u <t0kw0u@SZA0030883>
2022-12-14 15:57:35 +0100

    ZH

---
+++ T0KW0U_3ZH/Form1.cs
@@ -0,0 +1,15 @@
+namespace T0KW0U_3ZH
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
